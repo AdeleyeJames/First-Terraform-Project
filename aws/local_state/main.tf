80f377f44635f127ed516e488499122cc74c0d60
AdeleyeJames <77471273+AdeleyeJames@users.noreply.github.com>
2024-10-23 18:50:42 -0500

    Repo Update

---
+++ aws/local_state/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.71.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "Terraform 1st proj" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Sample"
+  }
+}
